bbc373b7be65d27d089103a9fb91d426e7015fe2
tsadoklevi <37581507+tsadoklevi@users.noreply.github.com>
2018-07-09 12:31:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "google_client_config" "default" {}
 
 resource "google_compute_subnetwork" "default" {
   name          = "${var.name}"
-  project       = "${length(var.region) > 0 ? var.project : data.google_client_config.default.region}"
+  region        = "${length(var.region) > 0 ? var.region : data.google_client_config.default.region}"
   network       = "${var.network}"
   ip_cidr_range = "${var.ip_cidr_range}"
   project       = "${length(var.project) > 0 ? var.project : data.google_client_config.default.project}"
